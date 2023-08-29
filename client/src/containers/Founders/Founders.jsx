d76af278dcdc5e5227501ec3260fac5c7cb4f5ee
Emanuel Hernandez <emanuelhc@Emanuels-MBP.lan>
2023-08-28 19:59:07 -0600

    Finishing second iteration

--- client/src/containers/Founders/Founders.jsx
+++ client/src/containers/Founders/Founders.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Founders.css';
-import founders from '../../assets/founders.png';
+import founders from '../../assets/founders3.png';
+import S from '../../assets/S.svg';
+import K from '../../assets/K.svg';
+import I from '../../assets/I.svg';
+import L from '../../assets/L.svg';
+import F from '../../assets/F.svg';
+import U from '../../assets/U.svg';
+import A from '../../assets/A.svg';
+
 
 const Founders = () => (
   <div className="skillfulai-founders">
     <div className="skillfulai_founders-content " id="founders">
       <h1 className="founders-heading">CORE TEAM <sub className="founders-subindex">(THE FOUNDERS)</sub></h1>
+      
       <img src={founders} alt="founders" className="founders-image1" />
     </div>
   </div>
